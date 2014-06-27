d85f6b7593b7468e75cc128241673ad4184bb846
bart <wojtowicz.bartlomiej@gmail.com>
2014-06-27 17:31:10 +0200

    sofa with rails emails preview

--- config/initializers/rails_email_preview.rb
+++ config/initializers/rails_email_preview.rb
@@ -1,7 +1,25 @@
+require 'rails_email_preview/integrations/comfortable_mexica_sofa'
+
+module RailsEmailPreview
+  module Integrations
+    module ComfortableMexicanSofa
+      module CmsVersionsCompatibility
+        def cms_snippet_class
+          Comfy::Cms::Snippet
+        end
+
+        def cms_site_class
+          Comfy::Cms::Site
+        end
+      end
+    end
+  end
+end
+
 require 'rails_email_preview'
 
 #= REP hooks and config
-#RailsEmailPreview.setup do |config|
+RailsEmailPreview.setup do |config|
 #
 #  # hook before rendering preview:
 #  config.before_render do |message, preview_class_name, mailer_action|
@@ -12,16 +30,15 @@ require 'rails_email_preview'
 #  end
 #
 #  # do not show Send Email button
-#  config.enable_send_email = false
+  config.enable_send_email = false
 #
 #  # You can specify a controller for RailsEmailPreview::ApplicationController to inherit from:
-#  config.parent_controller = 'Admin::ApplicationController' # default: '::ApplicationController'
-#end
+  config.parent_controller = 'Admin::ApplicationController' # default: '::ApplicationController'
+end
 
 #= REP + Comfortable Mexican Sofa integration
 #
 # # enable comfortable_mexican_sofa integration:
-require 'rails_email_preview/integrations/comfortable_mexica_sofa'
 
 Rails.application.config.to_prepare do
   # Render REP inside a custom layout (set to 'application' to use app layout, default is REP's own layout)
