a2300b73b4338653e87afb3280894382b2ed76e8
bart <wojtowicz.bartlomiej@gmail.com>
2014-07-09 16:56:50 +0200

    merge config files from rails 4.1.x

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,2 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key is used for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-Saapp::Application.config.secret_key_base = 'ede808aa0220506794556b84587f6aa1412753c418fad2030c853d85e77c17e38e796cff63db7aea820399d6f66957582885faab4463896ff8b7928cd7f8911f'
+# In RAILS 4.1.x this file is not needed anymore because everything should be loaded from secrets.yml but it does not???
+Saapp::Application.config.secret_key_base = Rails.application.secrets.secret_key_base
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Saapp::Application.config.session_store :cookie_store, key: '_saapp_session'
+Rails.application.config.session_store :cookie_store, key: '_saapp_session'
