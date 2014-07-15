2fb5557e0de86981513c3180b09748bbfc4131cb
stevo <blazejek@gmail.com>
2014-07-15 16:15:56 +0200

    Add foreign key in users table for teams #sap-60 comment

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140715133645) do
+ActiveRecord::Schema.define(version: 20140715141250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -206,6 +206,7 @@ ActiveRecord::Schema.define(version: 20140715133645) do
     t.datetime "updated_at"
     t.integer  "role",                   default: 0
     t.string   "display_name"
+    t.integer  "team_id",                             null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
