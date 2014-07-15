a3a9c1cd62355849bf754944c2a5b807c202aab2
stevo <blazejek@gmail.com>
2014-07-15 15:38:03 +0200

    Add display name to users #sap-60 comment

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140715131040) do
+ActiveRecord::Schema.define(version: 20140715133645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -205,6 +205,7 @@ ActiveRecord::Schema.define(version: 20140715131040) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "role",                   default: 0
+    t.string   "display_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
