7d91dfe3922189a8a357cfe7d85f1e1dbbebaf7b
stevo <blazejek@gmail.com>
2014-07-15 14:09:36 +0200

    Wizard model should store invitations and categories/arguments in addition to email #sap-60 comment

--- app/models/wizard.rb
+++ app/models/wizard.rb
@@ -1,11 +1,13 @@
 class Wizard
   include ActiveModel::Model
 
-  attr_accessor :email
+  attr_accessor :email, :invitations, :categories
 
   validates :email, presence: true, email: true
 
   def attributes=(attrs)
-    self.email = attrs[:email]
+    self.email = attrs.fetch(:email, '')
+    self.invitations = attrs.fetch(:invitations, [])
+    self.categories = attrs.fetch(:categories, [])
   end
 end
