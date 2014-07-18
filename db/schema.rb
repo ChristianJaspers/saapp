4477878edea778e0ec05b424819198be64ce2700
bart <wojtowicz.bartlomiej@gmail.com>
2014-07-18 03:22:34 +0200

    add attachment db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140715141934) do
+ActiveRecord::Schema.define(version: 20140718000954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -217,6 +217,10 @@ ActiveRecord::Schema.define(version: 20140715141934) do
     t.integer  "role",                   default: 0
     t.string   "display_name"
     t.integer  "team_id",                             null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
