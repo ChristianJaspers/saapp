03317891cee224b839ceb8e2b5724a7ebdf0a1c6
stevo <blazejek@gmail.com>
2014-07-21 16:26:45 +0200

    Allow accessing categories straight from team model #sap-94 comment

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ActiveRecord::Base
   belongs_to :owner, class_name: User, inverse_of: :categories
   has_many :features, inverse_of: :category
+
+  delegate :count, to: :features, prefix: true
 end
