cf3962f4de3e9b1ba8b1efa21336be15fe1cdf73
bart <wojtowicz.bartlomiej@gmail.com>
2014-07-22 15:21:39 +0200

    make required keys

--- config/initializers/figaro.rb
+++ config/initializers/figaro.rb
@@ -7,7 +7,9 @@ if Rails.env.production?
   required_env += [
     'S3_BUCKET_NAME',
     'AWS_ACCESS_KEY_ID',
-    'AWS_SECRET_ACCESS_KEY'
+    'AWS_SECRET_ACCESS_KEY',
+    'HOST',
+    'NOREPLY_EMAIL'
   ]
 end
 
