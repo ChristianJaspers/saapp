8bb927dd44275a2370b493df0c692860e2d79dff
stevo <blazejek@gmail.com>
2014-07-22 12:23:20 +0200

    Remove obsolete blank space #sap-96 comment

--- app/decorators/category_decorator.rb
+++ app/decorators/category_decorator.rb
@@ -3,7 +3,6 @@ class CategoryDecorator < ApplicationDecorator
 
   def publishing_link
     if object.archived_at
-
       link_to I18n.t('manager.categories.index.table.actions.unarchive_link'),
               h.manager_category_path(object, category: {archive: false}),
               method: :patch
