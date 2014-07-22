debbc98a658fa42a0cfc94038d3b15da79c8ab24
stevo <blazejek@gmail.com>
2014-07-22 14:03:17 +0200

    Add counter cache to categories (features) #sap-102 comment

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140722083833) do
+ActiveRecord::Schema.define(version: 20140722120039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,11 +51,12 @@ ActiveRecord::Schema.define(version: 20140722083833) do
   add_index "benefits", ["feature_id"], name: "index_benefits_on_feature_id", using: :btree
 
   create_table "categories", force: true do |t|
-    t.string   "name",        null: false
-    t.integer  "owner_id",    null: false
+    t.string   "name",           null: false
+    t.integer  "owner_id",       null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "archived_at"
+    t.integer  "features_count"
   end
 
   add_index "categories", ["name"], name: "index_categories_on_name", using: :btree
