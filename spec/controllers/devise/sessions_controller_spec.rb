ad2ad2c23b6e34425d774485aa0bccc39dc13f3f
bart <wojtowicz.bartlomiej@gmail.com>
2014-07-23 15:59:25 +0200

    add logout test for redirect

--- spec/controllers/devise/sessions_controller_spec.rb
+++ spec/controllers/devise/sessions_controller_spec.rb
@@ -10,6 +10,14 @@ describe Devise::SessionsController do
     end
   end
 
+  shared_examples 'user signs out' do
+    it 'user is redirect to root when logout' do
+      sign_in user
+      post :destroy
+      expect(response).to redirect_to '/'
+    end
+  end
+
   context 'user login' do
     let(:user) { create(:user) }
     let(:redirect_path) { '/' }
@@ -28,21 +36,20 @@ describe Devise::SessionsController do
     it_behaves_like 'user signs in'
   end
 
-  context 'user logout' do
-    let(:user) { create(:user) }
-    let(:redirect_path) { '/' }
-    it_behaves_like 'user signs in'
-  end
+  context 'logout' do
+    context 'user logout' do
+      let(:user) { create(:user) }
+      it_behaves_like 'user signs out'
+    end
 
-  context 'manager logout' do
-    let(:user) { create(:user, :manager) }
-    let(:redirect_path) { '/' }
-    it_behaves_like 'user signs in'
-  end
+    context 'manager logout' do
+      let(:user) { create(:user, :manager) }
+      it_behaves_like 'user signs out'
+    end
 
-  context 'admin logout' do
-    let(:user) { create(:user, :admin) }
-    let(:redirect_path) { '/admin' }
-    it_behaves_like 'user signs in'
+    context 'admin logout' do
+      let(:user) { create(:user, :admin) }
+      it_behaves_like 'user signs out'
+    end
   end
 end
