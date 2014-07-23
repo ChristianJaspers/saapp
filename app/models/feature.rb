4a2517d9ba94eef96df6e017064735d10b9e7adf
stevo <blazejek@gmail.com>
2014-07-23 15:17:18 +0200

    stubbed feature#rated implementation #SAP-95 comment

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -2,4 +2,8 @@ class Feature < ActiveRecord::Base
   belongs_to :owner, class_name: User, inverse_of: :features
   belongs_to :category, inverse_of: :features, counter_cache: true
   has_one :benefit, inverse_of: :feature, dependent: :destroy
+
+  def rated?
+    false #FIXME Implementation pending
+  end
 end
