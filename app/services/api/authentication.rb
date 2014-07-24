7c286dd52a7978c89713f5181a3fee77a394be6f
stevo <blazejek@gmail.com>
2014-07-24 16:11:29 +0200

    Fix specs related to Api Authentication #SAP-120 comment

--- app/services/api/authentication.rb
+++ app/services/api/authentication.rb
@@ -1,21 +1,23 @@
-class Api::Authentication
-  attr_reader :param_token
-  delegate :user, to: :api_token, allow_nil: true
+module Api
+  class Authentication
+    attr_reader :param_token
+    delegate :user, to: :api_token, allow_nil: true
 
-  def initialize(param_token)
-    @param_token = param_token
-  end
+    def initialize(param_token)
+      @param_token = param_token
+    end
 
-  def authenticate!
-    !!user
-  end
+    def authenticate!
+      !!user
+    end
 
-  private
+    private
 
-  def api_token
-    @api_token ||= begin
-      token = ApiToken.for_access_token(param_token).includes(:user).first
-      token if token && Devise.secure_compare(token.access_token, param_token)
+    def api_token
+      @api_token ||= begin
+        token = ApiToken.for_access_token(param_token).includes(:user).first
+        token if token && Devise.secure_compare(token.access_token, param_token)
+      end
     end
   end
 end
