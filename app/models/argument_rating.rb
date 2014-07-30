869475d514340ade62f82eb66acb7952b2459ec9
stevo <blazejek@gmail.com>
2014-07-30 09:48:02 +0200

    Cache rating in argument after adding it #SAP-135 comment

--- app/models/argument_rating.rb
+++ app/models/argument_rating.rb
@@ -2,6 +2,8 @@ class ArgumentRating < ActiveRecord::Base
   belongs_to :rater, class_name: User, inverse_of: :ratings
   belongs_to :argument, inverse_of: :ratings
 
+  validates :rater_id, :argument_id, :rating, presence: true
+
   enum rating: {
       low: 1,
       medium: 2,
