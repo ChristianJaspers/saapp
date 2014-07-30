ba2320e3cfc1b36992a82c52f1461a4e98259b3c
bart <wojtowicz.bartlomiej@gmail.com>
2014-07-30 13:08:44 +0200

    specs for bussines process

---
+++ spec/services/manager/users/create_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe Manager::Users::Create do
+  describe '.call' do
+    let(:user) { build(:user, email: 'fake@email.com') }
+    let(:parameter_object) { double(user: user) }
+    let(:perform) { described_class.call(parameter_object) }
+
+    before { allow(ApplicationMailer).to receive(:user_invitation) }
+
+    context 'after perform' do
+      before { perform }
+
+      it { expect(ApplicationMailer).to have_received(:user_invitation).with(User.user.last) }
+      it { expect(user.reload.instance_variable_get('@skip_confirmation_notification')).to be_truthy }
+      it { expect(User).to exist.with(email: 'fake@email.com') }
+    end
+  end
+end
---
+++ spec/services/manager/users/update_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe Manager::Users::Update do
+  describe '.call' do
+    let(:user) { create(:user) }
+    let(:params) do
+      {
+        user: {
+          manager: '1'
+        }
+      }.with_indifferent_access
+    end
+    let(:parameter_object) { double(user: user, params: params) }
+    let(:perform) { described_class.call(parameter_object) }
+
+    context 'after perform' do
+      it { expect { perform }.to change{ user.reload.role }.from('user').to('manager') }
+      it { expect { perform }.to change{ user.reload.reset_password_token }.from(nil) }
+    end
+  end
+end
