35a87d09d79674b2aaf596db82fc349d2c79e3e5
bart <wojtowicz.bartlomiej@gmail.com>
2014-07-31 00:04:27 +0200

    team serializer

---
+++ app/serializers/api/team_serializer.rb
@@ -0,0 +1,24 @@
+class Api::TeamSerializer < ActiveModel::Serializer
+  self.root = false
+
+  attributes :product_groups
+
+  has_many :sales_representatives, serializer: Api::UserSerializer, key: 'users'
+  has_many :arguments, serializer: Api::ArgumentSerializer
+
+  def arguments
+    object.arguments.order(:id)
+  end
+
+  def product_groups
+    object.product_groups.order(:id).map.with_index do |product_group, index|
+      Api::ProductGroupSerializer.new(product_group, root: false, color_iterator: color_iterator, position: index + 1)
+    end
+  end
+
+  private
+
+  def color_iterator
+    @color_iterator ||= ColorCycleIterator.new
+  end
+end
