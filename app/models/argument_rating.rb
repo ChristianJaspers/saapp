574b9ae758d1d5888d33646cdcf1b6486843c787
stevo <blazejek@gmail.com>
2014-07-30 09:28:53 +0200

    Define rating as enum field #SAP-135 comment

--- app/models/argument_rating.rb
+++ app/models/argument_rating.rb
@@ -1,4 +1,10 @@
 class ArgumentRating < ActiveRecord::Base
   belongs_to :rater, class_name: User, inverse_of: :ratings
   belongs_to :argument, inverse_of: :ratings
+
+  enum rating: {
+      low: 1,
+      medium: 2,
+      high: 3
+  }
 end
