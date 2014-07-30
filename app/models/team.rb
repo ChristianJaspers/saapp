4c9546387fcde6128d1f5d72034c6d6747fa3b29
bart <wojtowicz.bartlomiej@gmail.com>
2014-07-31 00:00:24 +0200

    add associations to tema

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,8 +3,10 @@ class Team < ActiveRecord::Base
 
   belongs_to :company, inverse_of: :teams
   has_many :users, inverse_of: :team
+  has_many :sales_representatives, -> { where role: User.roles['user'] }, class_name: User
   has_many :product_groups, through: :users
   has_many :scorings, class_name: Gamification::Scoring, through: :users
+  has_many :arguments, through: :product_groups
 
   def goal_score
     [max_team_score, average_team_score * 2].max
