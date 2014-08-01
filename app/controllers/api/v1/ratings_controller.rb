aa69425ced231df8c61ba8551de7b47d989ffcdb
stevo <blazejek@gmail.com>
2014-08-01 12:03:20 +0200

    Refactor API Rating service object

--- app/controllers/api/v1/ratings_controller.rb
+++ app/controllers/api/v1/ratings_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::RatingsController < Api::RestrictedApplicationController
   def create
-    if (rate = Api::RateArgument.call(current_user: current_user, params: params)).success?
+    if (rate = Api::RateArgument.call(self)).success?
       render_json(rate.result, 201, serializer: Api::ArgumentRatingSerializer)
     else
       render_fail_json(:forbidden)
