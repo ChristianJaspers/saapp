8d9f2895a051480aeb8582e116a9305f438e9fa1
bart <wojtowicz.bartlomiej@gmail.com>
2014-08-07 19:38:33 +0200

    add user profile manager

---
+++ app/controllers/manager/profiles_controller.rb
@@ -0,0 +1,20 @@
+class Manager::ProfilesController < Manager::ManagerController
+  expose(:sales_reps_count) { current_user.team.sales_representatives.count }
+
+  layout 'manager'
+
+  def update
+    current_user.attributes = user_params
+    if current_user.save
+      redirect_to edit_manager_profile_path, notice: t('manager.profile.update.notifications.success')
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:display_name, :address)
+  end
+end
