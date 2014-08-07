ee79c2a2f0289e470c14888194acdb0df5da1bbd
bart <wojtowicz.bartlomiej@gmail.com>
2014-08-07 19:44:52 +0200

    updating user profile

---
+++ spec/controllers/manager/profiles_controller_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+describe Manager::ProfilesController do
+  subject { controller }
+  render_views
+
+  describe '#update' do
+    include_context 'manager is logged in'
+
+    let(:call_request) { patch(:update, user: attributes) }
+
+    context 'updating user' do
+      let(:attributes) do
+        {
+          display_name: 'Abc',
+          address: 'Home'
+        }
+      end
+
+      it { expect { call_request }.to change { manager.reload.display_name }.to('Abc') }
+      it { expect { call_request }.to change { manager.reload.address }.to('Home') }
+      it_behaves_like 'an action redirecting to', -> { edit_manager_profile_path }
+    end
+
+    context 'changing other params' do
+      let(:attributes) do
+        {
+          role: 'manager'
+        }
+      end
+
+      it 'is not allowed' do
+        expect { call_request }.to_not change { manager.reload.role }
+      end
+    end
+  end
+end
