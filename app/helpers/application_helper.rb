d38a42b70af6ac8a4fc9303b21f2dceb8e30fe25
bart <wojtowicz.bartlomiej@gmail.com>
2014-08-07 17:58:19 +0200

    available locales dropdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,16 @@ module ApplicationHelper
   def localized_cms_page_path(cms_page)
     "/#{cms_page.site.path}/#{cms_page.full_path}".squeeze("/")
   end
+
+  def localized_current_path(locale)
+    if locale == I18n.default_locale
+      '/'
+    else
+      "/#{locale}"
+    end
+  end
+
+  def locale_flag_filename(locale)
+    "locales/#{locale}.png"
+  end
 end
