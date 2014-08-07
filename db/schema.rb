9820920aab5ab2c2e625b805b9dbf18b5cafade2
bart <wojtowicz.bartlomiej@gmail.com>
2014-08-07 19:33:30 +0200

    add address to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140807100436) do
+ActiveRecord::Schema.define(version: 20140807170234) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -237,6 +237,7 @@ ActiveRecord::Schema.define(version: 20140807100436) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.date     "remove_at"
+    t.text     "address"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
