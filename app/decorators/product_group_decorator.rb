9896f15c7941eb153dbcc8620a5185b9f0b49a10
arekjanik <arek.mp@gmail.com>
2014-08-14 14:05:32 +0200

    applying feedback

--- app/decorators/product_group_decorator.rb
+++ app/decorators/product_group_decorator.rb
@@ -1,35 +1,32 @@
 class ProductGroupDecorator < ApplicationDecorator
   def publishing_link
     if object.archived_at
-      link_to '',
-              h.manager_product_group_path(object, product_group: {archive: false}),
-              title: I18n.t('manager.product_groups.index.table.actions.unarchive_link'),
-              class: 'btn-icon icon-publish',
-              method: :patch
+      icon_link_to 'publish', h.manager_product_group_path(object, product_group: {archive: false}),
+                   title: I18n.t('manager.product_groups.index.table.actions.unarchive_link'),
+                   method: :patch
     else
-      link_to '',
-              h.manager_product_group_path(object, product_group: {archive: true}),
-              title: I18n.t('manager.product_groups.index.table.actions.archive_link'),
-              class: 'btn-icon icon-unpublish',
-              method: :patch
+      icon_link_to 'unpublish', h.manager_product_group_path(object, product_group: {archive: true}),
+                   title: I18n.t('manager.product_groups.index.table.actions.archive_link'),
+                   method: :patch
     end
   end
 
   def removal_link
     if object.removable_by?(current_user)
-      link_to '',
-              h.manager_product_group_path(object.id),
-              title: I18n.t('manager.product_groups.index.table.actions.destroy_link'),
-              class: 'btn-icon icon-trash',
-              method: :delete,
-              data: {confirm: I18n.t('manager.product_groups.index.table.actions.destroy_confirmation')}
+      icon_link_to 'trash', h.manager_product_group_path(object.id),
+                   title: I18n.t('manager.product_groups.index.table.actions.destroy_link'),
+                   method: :delete,
+                   data: {confirm: I18n.t('manager.product_groups.index.table.actions.destroy_confirmation')}
     end
   end
 
   def edit_link
-    link_to '',
-            h.edit_manager_product_group_path(object.id),
-            title: I18n.t('manager.product_groups.index.table.actions.edit_link'),
-            class: 'btn-icon icon-edit'
+    icon_link_to 'edit', h.edit_manager_product_group_path(object.id),
+                 title: I18n.t('manager.product_groups.index.table.actions.edit_link')
+  end
+
+  def icon_link_to(icon_name, path, options)
+    options[:class] = "btn-icon icon-#{icon_name}"
+    link_to '', path, options
   end
 end
