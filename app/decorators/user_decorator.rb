17d7c5d36d444d34c4ece2ad3ed93283da7fa542
arekjanik <arek.mp@gmail.com>
2014-08-18 09:59:05 +0200

    my account styling #sap-121 #comment

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -3,7 +3,7 @@ class UserDecorator < ApplicationDecorator
     link_to '',
             h.manager_user_path(object.id),
             title: I18n.t('manager.users.index.table.actions.destroy_link'),
-            class: 'btn-icon icon-trash',
+            class: 'btn-icon icon-trash btn-dark',
             method: :delete,
             data: {confirm: I18n.t('manager.users.index.table.actions.destroy_confirmation')}
   end
