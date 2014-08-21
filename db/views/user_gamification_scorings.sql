b9ec233cb308e3ac763d541995b56d687a1eb0f2
bart <wojtowicz.bartlomiej@gmail.com>
2014-08-21 16:04:28 +0200

    change db view

--- db/views/user_gamification_scorings.sql
+++ db/views/user_gamification_scorings.sql
@@ -6,7 +6,8 @@ SELECT
   usr.email,
   usr.display_name,
   scoring.amount,
-  scoring.event_name
+  scoring.event_name,
+  scoring.created_at
 FROM users AS usr
 INNER JOIN teams AS t ON t.id = usr.team_id
 LEFT OUTER JOIN gamification_scorings AS scoring ON scoring.beneficiary_id = usr.id
