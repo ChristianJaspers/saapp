2fa5736c3f166e2bcdb140e93c5c0cadd2dbb54a
bart <wojtowicz.bartlomiej@gmail.com>
2014-08-25 17:03:44 +0200

    show not rated args last

--- app/controllers/manager/reports/argument_rankings_controller.rb
+++ app/controllers/manager/reports/argument_rankings_controller.rb
@@ -1,5 +1,5 @@
 class Manager::Reports::ArgumentRankingsController < Manager::ManagerController
   layout 'manager'
 
-  expose(:arguments) { current_user.team.arguments.order(cached_rating: :desc) }
+  expose(:arguments) { current_user.team.arguments.order('cached_rating DESC NULLS LAST') }
 end
