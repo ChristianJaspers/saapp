8c005f0b0cb2a2822bdb708da276715787761f6f
bart <wojtowicz.bartlomiej@gmail.com>
2014-09-25 18:32:59 +0200

    remove paremter object and use own user instance

--- app/services/manager/create_user.rb
+++ app/services/manager/create_user.rb
@@ -11,11 +11,12 @@ class Manager::CreateUser < BusinessProcess::Base
   private
 
   def create_or_restore_user
+    @user_to_process = user
     restore_user = Manager::RestoreUser.new(user.email, current_user)
 
     if restore_user.find_user_to_restore_by_email
       if restore_user.call
-         @parameter_object[:user] = restore_user.restoree
+        @user_to_process = restore_user.restoree
       else
         nil
       end
@@ -25,15 +26,19 @@ class Manager::CreateUser < BusinessProcess::Base
   end
 
   def create_user
-    user.skip_confirmation_notification!
-    user.save
+    user_to_process.skip_confirmation_notification!
+    user_to_process.save
   end
 
   def update_remote_subscription
-    SubscriptionUpdater.call(user: user).success?
+    SubscriptionUpdater.call(user: user_to_process).success?
   end
 
   def send_invitation
-    ApplicationMailer.user_invitation(user)
+    ApplicationMailer.user_invitation(user_to_process)
   end
+
+  private
+
+  attr_reader :user_to_process
 end
