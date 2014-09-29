41518d1ea7137a9e0d695d2de119dfc4d57c8f40
bart <wojtowicz.bartlomiej@gmail.com>
2014-09-29 19:47:59 +0200

    if user had remote subscription and is not active then logut

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,11 @@
 class SessionsController < Devise::SessionsController
   def create
     super do |resource|
-      # if company_subscription.any_remote_subscription
-      #   unless company_subscription.active_remote_subscription
-      #     sign_out :user
-      #   end
-      # end
+      if company_subscription.any_remote_subscription
+        unless company_subscription.active_remote_subscription
+          sign_out :user
+        end
+      end
     end
   end
 end
