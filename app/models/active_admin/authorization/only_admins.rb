2ee0aa43d394da3c0cc0936ecd05ba4d5abfa916
bart <wojtowicz.bartlomiej@gmail.com>
2014-10-07 17:28:37 +0200

    authorize cms editor only to cms pages

--- app/models/active_admin/authorization/only_admins.rb
+++ app/models/active_admin/authorization/only_admins.rb
@@ -2,7 +2,26 @@ module ActiveAdmin
   module Authorization
     class OnlyAdmins < ActiveAdmin::AuthorizationAdapter
       def authorized?(action, subject = nil)
+        case subject
+          when ActiveAdmin::Page
+            if subject.name == 'CMS' || subject.name == 'Dashboard'
+              is_cms_editor?
+            else
+              is_admin?
+            end
+        else
+          is_admin?
+        end
+      end
+
+      private
+
+      def is_admin?
         user.admin?
+      end
+
+      def is_cms_editor?
+         is_admin? || user.cms_editor?
       end
     end
   end
--- config/initializers/comfortable_mexican_sofa.rb
+++ config/initializers/comfortable_mexican_sofa.rb
@@ -2,9 +2,8 @@
 #
 module CmsDeviseAuth
   def authenticate
-    unless current_user && current_user.admin?
-      redirect_to new_user_session_path
-    end
+    return true if current_user && (current_user.cms_editor? || current_user.admin?)
+    redirect_to new_user_session_path
   end
 end
 
