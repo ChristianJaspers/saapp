242e35a7cb8403f4d8be0ed93387f29c79ea1500
bart <wojtowicz.bartlomiej@gmail.com>
2014-10-09 16:22:07 +0200

    force https if setting allowed for it

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  force_ssl if: :ssl_configured?
+
   include LanguageAutoredirect
 
   decent_configuration do
@@ -54,5 +56,11 @@ class ApplicationController < ActionController::Base
     {
       locale: (I18n.locale == I18n.default_locale) ? nil : I18n.locale
     }
+  end
+
+  def ssl_configured?
+    # TO BE CHANGE WHEN MOBILE GUYS SET API URL FOR HTTPS PROTOCOL
+    # !Rails.env.development?
+    false
   end
 end
