5afd198cff01ac9abafc14af5e7602308110ea9b
bart <wojtowicz.bartlomiej@gmail.com>
2014-11-07 20:27:25 +0100

    test destroying argument

---
+++ spec/services/manager/destroy_argument_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe Manager::DestroyArgument do
+  describe '.call' do
+    let(:product_group_company_1) { create(:product_group) }
+    let(:product_group_company_2) { create(:product_group) }
+    let(:current_user) { product_group_company_1.owner }
+    let(:other_company_user) { product_group_company_2.owner }
+    let!(:argument_company_1) { create(:argument, product_group: product_group_company_1, owner: current_user) }
+    let!(:argument_company_2) { create(:argument, product_group: product_group_company_2, owner: other_company_user) }
+
+    let(:parameter_object) { double(current_user: current_user, params: params) }
+    let(:perform) { described_class.call(parameter_object).success? }
+
+    context 'deleting argument from your company' do
+      let(:params) { {id: argument_company_1.id} }
+
+      it { expect(perform).to be_truthy }
+    end
+
+    context 'deleting argument from different company' do
+      let(:params) { {id: argument_company_2.id} }
+
+      it { expect(perform).to be_falsey }
+    end
+  end
+end
