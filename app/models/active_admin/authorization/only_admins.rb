955c6389af5237d757b76aac2c0beb39c9b3f3da
bart <wojtowicz.bartlomiej@gmail.com>
2014-12-02 13:01:59 +0100

    allow seo guys to admin seo panel

--- app/models/active_admin/authorization/only_admins.rb
+++ app/models/active_admin/authorization/only_admins.rb
@@ -2,6 +2,8 @@ module ActiveAdmin
   module Authorization
     class OnlyAdmins < ActiveAdmin::AuthorizationAdapter
       def authorized?(action, subject = nil)
+        return is_cms_editor? if access_to_seo?(action, subject)
+
         case subject
           when ActiveAdmin::Page
             if subject.name == 'CMS' || subject.name == 'Dashboard'
@@ -15,6 +17,10 @@ module ActiveAdmin
       end
 
       private
+
+      def access_to_seo?(action, subject)
+        subject == Comfy::Cms::Site || subject == Comfy::Cms::Page || subject.is_a?(Comfy::Cms::Page)
+      end
 
       def is_admin?
         user.admin?
